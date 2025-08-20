5e2852b3baa9a1d626ed193d4494bff91fde05f7
Erwan Chesneau <erwanchesneau@gmail.com>
2025-08-20 22:32:17 +0200

    add version in init

---
+++ src/trading_system/__init__.py
@@ -0,0 +1,11 @@
+"""
+Trading System - Système de trading algorithmique hybride
+Copyright (c) 2024 Votre Nom. Licence MIT.
+"""
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0+unknown"
+
+__all__ = ["__version__"]
