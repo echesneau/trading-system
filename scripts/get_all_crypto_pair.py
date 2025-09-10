d99bb9f8f2915afe772c893914a20b7627d3898d
Erwan Chesneau <erwanchesneau@gmail.com>
2025-09-10 22:12:22 +0200

    script: get_all_crypto_pair.py for EUR and USD

--- scripts/get_all_crypto_pair.py
+++ scripts/get_all_crypto_pair.py
@@ -7,5 +7,8 @@ if __name__ == "__main__":
     # Liste des symboles dispo
     pairs = list(markets.keys())
     pairs_euro = [pair for pair in pairs if pair.endswith("/EUR")]
+    pair_usd = [pair for pair in pairs if pair.endswith("/USDT")]
     print(pairs_euro)  # affiche les 20 premières
     print(f"\nNombre total de paires en Euro: {len(pairs_euro)}")
+    print(pair_usd)  # affiche les 20 premières
+    print(f"\nNombre total de paires en USD: {len(pair_usd)}")
