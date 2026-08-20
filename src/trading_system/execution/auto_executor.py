2d49692c96b3f939312de9d2433ec9a252a698db
CHESNEAU Erwan <erwan.chesneau@cstb.fr>
2026-08-20 14:02:50 +0200

    inversion ordre des actions. dans auto_executor.py

--- src/trading_system/execution/auto_executor.py
+++ src/trading_system/execution/auto_executor.py
@@ -28,18 +28,7 @@ class AutoExecutor:
             "errors": []
         }
 
-        # 1. Exécuter les BUY
-        for sig in random.sample(report["buy_signals"], len(report["buy_signals"])):
-            if not sig['ticker'].endswith("EUR"):
-                executions["errors"].append({"signal": sig, "error": "Transaction en € uniquement."})
-            else:
-                try:
-                    result = self.execute_buy(sig)
-                    executions["executed"].append(result)
-                except Exception as e:
-                    executions["errors"].append({"signal": sig, "error": str(e)})
-
-        # 2. Exécuter les SELL
+        # 1. Exécuter les SELL
         for sig in random.sample(report["sell_signals"], len(report["sell_signals"])):
             if not sig['ticker'].endswith("EUR"):
                 executions["errors"].append({"signal": sig, "error": "Transaction en € uniquement."})
@@ -48,6 +37,17 @@ class AutoExecutor:
                     result = self.execute_sell(sig)
                     if result is not None:
                         executions["executed"].append(result)
+                except Exception as e:
+                    executions["errors"].append({"signal": sig, "error": str(e)})
+
+        # 2. Exécuter les BUY
+        for sig in random.sample(report["buy_signals"], len(report["buy_signals"])):
+            if not sig['ticker'].endswith("EUR"):
+                executions["errors"].append({"signal": sig, "error": "Transaction en € uniquement."})
+            else:
+                try:
+                    result = self.execute_buy(sig)
+                    executions["executed"].append(result)
                 except Exception as e:
                     executions["errors"].append({"signal": sig, "error": str(e)})
 
